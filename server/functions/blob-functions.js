3b7fe17ac3939abe59b62fd469f488087bd93d3e
Griffin Overmyer <griffinovermyer@gmail.com>
2024-02-18 20:17:18 -0500

    successfully added a photo to blob storage from server

--- server/functions/blob-functions.js
+++ server/functions/blob-functions.js
@@ -1,16 +1,26 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 require("dotenv").config({ path: __dirname + "../../.env" });
+const fs = require("fs");
 
 const connStr = String(process.env.BLOB_CONNECTION);
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(connStr);
 
 async function main() {
-  let i = 1;
-  let containers = blobServiceClient.listContainers();
-  for await (const container of containers) {
-    console.log(`Container ${i++}: ${container.name}`);
-  }
+  const containerClient = blobServiceClient.getContainerClient("pic-storage-container");
+
+  let base64_image;
+  fs.readFile(__dirname + "/../" + "assets/" + "static-logo.png", async (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const blockBlobClient = containerClient.getBlockBlobClient("static-logo.png");
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+
+    console.log("blob created successfully");
+  });
 }
 
 main();
