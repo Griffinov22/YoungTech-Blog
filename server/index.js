383dedfbe1547ec90c9dfd62449fa650fac8a17b
Griffin Overmyer <griffinovermyer@gmail.com>
2024-09-16 22:28:27 -0400

    host backend on render. Cors setup for backend. Frontend configured to talk to backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ const {
   updatePost,
 } = require("./functions/todos-functions");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(fileUpload());
